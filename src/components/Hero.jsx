bdce01555465e9d3211832413d23583304d1a15b
Sotham <lanches.thomas@gmail.com>
2023-08-05 16:50:24 +0200

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+        return (
+                <header className="w-full flex justify-center items-center flex-col">
+                        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                                <img
+                                        src={logo}
+                                        alt=""
+                                        className="w-28 object-contain"
+                                />
+                                <button
+                                        type="button"
+                                        onClick={() =>
+                                                window.open(
+                                                        'https://github.com/LanchesThomas'
+                                                )
+                                        }
+                                        className="black_btn"
+                                >
+                                        Github
+                                </button>
+                        </nav>
+                        <h1 className="head_text">
+                                Résumez vos articles avec <br className='max-md:hidden'/>
+                                <span className="orange_gradient">
+                                        OpenAI GPT-4
+                                </span>
+                        </h1>
+                        <h2 className='desc'>Simplifiez votre lecture grâce à un résumé de texte open source
+        qui transforme de longs articles en résumés clairs et concis</h2>
+                </header>
+        );
+};
+
+export default Hero;
